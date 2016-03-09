9acb56db413afea6ed65c2cc913f082ed5e84875
zwallace04 <zachary.wallace04@gmail.com>
2016-03-09 12:43:42 -0500

    Made some cool changes.

--- 585burnsafety_2/LivingRoomImages/index.js
+++ 585burnsafety_2/LivingRoomImages/index.js
@@ -53,3 +53,33 @@ var incrementCnt = function(){
     $("#levelWin").css('visibility', 'visible');
   }
 }
+//this is for the timer
+function countdown( elementName, minutes, seconds )
+{
+    var element, endTime, hours, mins, msLeft, time;
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            alert('game over!');
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            $("#timer").html(hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    //element = document.getElementById( elementName );
+    //alert('here: '+element);
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+countdown( "timer", 1, 15 );
